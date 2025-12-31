508a824c8c319343c2abc2e0eb2b4e1aeba32987
sitaram-a <sitaram.hembrom123@gmail.com>
2026-01-01 00:14:23 +0530

    Add CORS for InfinityFree frontend

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "https://sitaramaccount.infinityfreeapp.com"
+}));
+
 app.use(express.json());
 
 // MySQL connection
