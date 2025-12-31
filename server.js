1470de57dc3a2627145ea77a1db5fb5d7c003b72
sitaram-a <sitaram.hembrom123@gmail.com>
2026-01-01 00:44:42 +0530

    Fix MySQL connection for InfinityFree

--- server.js
+++ server.js
@@ -19,7 +19,13 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
+
+
 
 
 db.connect((err) => {
